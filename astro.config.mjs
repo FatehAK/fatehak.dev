db3d41d51207daf7f3f755b1e638eed9a5b86f1f
fa7ehak <fa7ehak@gmail.com>
2023-01-14 00:48:14 +0530

    build: Use env for build mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,25 @@ import ViteLinariaPlugin from '@linaria/vite';
 import getTargetBrowsers from 'browserslist-to-esbuild';
 import { APP_CONFIG, COMPRESSION_CONFIG } from './appConfig';
 
+const isProd = process.env.BUILD === 'production';
 export default defineConfig({
   site: APP_CONFIG.PROD_BASE_URL,
-  integrations: [AstroReactPlugin(), AstroCompress(COMPRESSION_CONFIG)],
+  integrations: [AstroReactPlugin(), isProd && AstroCompress(COMPRESSION_CONFIG)],
   server: { port: 3000, host: true },
   vite: {
     plugins: [
       ViteLinariaPlugin({
         include: ['**/*.styles.js', '**/*.jsx'],
         classNameSlug: (hash, title) => `${paramCase(title)}_${hash}`,
+        ...(!isProd && {
+          sourceMap: true,
+        }),
       }),
     ],
     build: {
       target: getTargetBrowsers(),
+      minify: isProd ? 'esbuild' : false,
+      sourcemap: isProd ? 'hidden' : true,
       rollupOptions: {
         output: {
           entryFileNames: '[name].[hash].js',

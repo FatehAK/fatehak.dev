85ad3cbddd3583277bd348f640dd341a2ff774de
fa7ehak <fa7ehak@gmail.com>
2023-01-24 00:13:00 +0530

    chore: Cleanup color tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,36 +10,36 @@ module.exports = {
       md: { max: '700px' },
       sm: { max: '450px' },
     },
-    extend: {
-      fontFamily: {
-        body: ['Inter', 'sans-serif'],
+    fontFamily: {
+      body: ['Inter', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      bg: {
+        body: 'rgb(var(--color-bg-body) / <alpha-value>)',
+        code: 'rgb(var(--color-bg-code) / <alpha-value>)',
+        selection: 'rgb(var(--color-bg-selection) / <alpha-value>)',
       },
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        bg: {
-          body: 'rgb(var(--color-bg-body) / <alpha-value>)',
-          code: 'rgb(var(--color-bg-code) / <alpha-value>)',
-          selection: 'rgb(var(--color-bg-selection) / <alpha-value>)',
-        },
-        primary: 'rgb(var(--color-primary) / <alpha-value>)',
-        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
-        text: {
-          heading: 'rgb(var(--color-text-heading) / <alpha-value>)',
-          body: 'rgb(var(--color-text-body) / <alpha-value>)',
-          bold: 'rgb(var(--color-text-bold) / <alpha-value>)',
-          muted: 'rgb(var(--color-text-muted) / <alpha-value>)',
-          code: 'rgb(var(--color-text-code) / <alpha-value>)',
-          link: 'rgb(var(--color-secondary) / <alpha-value>)',
-          selection: 'rgb(var(--color-text-selection) / <alpha-value>)',
-        },
-        border: {
-          code: 'rgb(var(--color-border-code) / <alpha-value>)',
-          nav: 'rgb(var(--color-nav-border) / <alpha-value>)',
-        },
-        gradient: {
-          shade1: 'rgb(var(--gradient-shade-1) / <alpha-value>)',
-        },
+      primary: {
+        DEFAULT: 'rgb(var(--color-primary) / <alpha-value>)',
+        light: 'rgb(var(--color-primary-light) / <alpha-value>)',
+        dark: 'rgb(var(--color-primary-dark) / <alpha-value>)',
+      },
+      secondary: {
+        DEFAULT: 'rgb(var(--color-secondary) / <alpha-value>)',
+      },
+      text: {
+        heading: 'rgb(var(--color-text-heading) / <alpha-value>)',
+        body: 'rgb(var(--color-text-body) / <alpha-value>)',
+        bold: 'rgb(var(--color-text-bold) / <alpha-value>)',
+        muted: 'rgb(var(--color-text-muted) / <alpha-value>)',
+        code: 'rgb(var(--color-text-code) / <alpha-value>)',
+        link: 'rgb(var(--color-secondary) / <alpha-value>)',
+        selection: 'rgb(var(--color-text-selection) / <alpha-value>)',
+      },
+      border: {
+        code: 'rgb(var(--color-border-code) / <alpha-value>)',
       },
     },
   },

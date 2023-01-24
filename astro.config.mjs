8379524e61359a1c8bb4fceab32e086612a1cc77
fa7ehak <fa7ehak@gmail.com>
2023-01-24 17:03:32 +0530

    feat: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,16 @@ import AstroCompress from 'astro-compress';
 import AstroTailwindPlugin from '@astrojs/tailwind';
 import getTargetBrowsers from 'browserslist-to-esbuild';
 import AstroPrefetch from '@astrojs/prefetch';
+import AstroSitemap from '@astrojs/sitemap';
 import { APP_CONFIG, COMPRESSION_CONFIG } from './appConfig';
-const isProd = process.env.BUILD === 'production';
 
-// https://astro.build/config
+const isProd = process.env.BUILD === 'production';
 export default defineConfig({
   site: APP_CONFIG.SITE_URL,
   integrations: [
-    AstroTailwindPlugin({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
+    AstroTailwindPlugin({ config: { applyBaseStyles: false } }),
     AstroPrefetch({ throttle: 3 }),
+    AstroSitemap(),
     isProd && AstroCompress(COMPRESSION_CONFIG),
   ],
   server: {

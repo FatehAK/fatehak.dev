63fd471ee73093f92053ef7c8f439abae8fe31c2
fa7ehak <fa7ehak@gmail.com>
2023-02-11 14:12:29 +0530

    refactor: Compute reading time only for blog content

--- scripts/remark-reading-time.js
+++ scripts/remark-reading-time.js
@@ -2,9 +2,14 @@ import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 
 export default function () {
-  return function (tree, { data }) {
-    const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage);
-    data.astro.frontmatter.readingTime = readingTime.text;
+  return function (tree, file) {
+    const filePath = file.history[0];
+
+    const isBlogContent = filePath.match(/\/content\/blog\//);
+    if (isBlogContent) {
+      const textOnPage = toString(tree);
+      const readingTime = getReadingTime(textOnPage);
+      file.data.astro.frontmatter.readingTime = readingTime.text;
+    }
   };
 }

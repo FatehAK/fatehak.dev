d0647027c19b67b2336a0d1d667d7d27ec16e6d3
fa7ehak <fa7ehak@gmail.com>
2023-02-12 02:24:47 +0530

    feat: Add snippets slug page

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,6 +19,23 @@ const blogCollection = defineCollection({
     .strict(),
 });
 
+const snippetsCollection = defineCollection({
+  schema: z
+    .object({
+      draft: z.boolean().default(false),
+      title: z.string(),
+      highlight: z.union([z.string(), z.array(z.string())]).default(''),
+      date: z.string().transform(str => new Date(str)),
+      lastMod: z
+        .string()
+        .transform(str => new Date(str))
+        .optional(),
+      tags: z.array(z.string()).default([]),
+    })
+    .strict(),
+});
+
 export const collections = {
   blog: blogCollection,
+  snippets: snippetsCollection,
 };

9fd6a2618b96642f9ed1c22a0e234835e1c41c2e
fa7ehak <fa7ehak@gmail.com>
2023-02-24 23:45:05 +0530

    refactor: Add trailing slash to url paths and prefetch pagination urls

--- src/pages/_notes/config.js
+++ src/pages/_notes/config.js
@@ -3,18 +3,18 @@ export const notesData = [
     name: 'Snippets',
     description: 'Code snippets for simple to complex tasks.',
     icon: 'code',
-    path: '/snippets',
+    path: '/snippets/',
   },
   {
     name: 'Tech Stack',
     description: 'Tech and perf metrics used to build this website.',
     icon: 'wand-spark',
-    path: '/tech-stack',
+    path: '/tech-stack/',
   },
   {
     name: 'Dev Tools',
     description: 'A curated collection of resources for development.',
     icon: 'screwdriver-wrench',
-    path: '/dev-tools',
+    path: '/dev-tools/',
   },
 ];

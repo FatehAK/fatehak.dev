9c38bd6ee4f1af419414ae812d75b42e1bd7baeb
fa7ehak <fa7ehak@gmail.com>
2023-02-24 22:53:25 +0530

    docs: Add tech stack page

--- src/pages/_notes/config.js
+++ src/pages/_notes/config.js
@@ -6,8 +6,14 @@ export const notesData = [
     path: '/snippets',
   },
   {
+    name: 'Tech Stack',
+    description: 'Tech and perf metrics used to build this website.',
+    icon: 'wand-spark',
+    path: '/tech-stack',
+  },
+  {
     name: 'Dev Tools',
-    description: 'A collection of useful tools for development.',
+    description: 'A curated collection of resources for development.',
     icon: 'screwdriver-wrench',
     path: '/dev-tools',
   },
